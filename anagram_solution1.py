20e22770311911d2cad87868cd6b48d26d31cc07
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-25 11:49:44 +0100

    A program to check if two strings are anagrams

---
+++ anagram_solution1.py
@@ -0,0 +1,20 @@
+def anagram(s1, s2):
+    list1 = list(s1)
+    list2 = list(s2)
+
+    list1.sort()
+    list2.sort()
+
+    pos = 0
+    matches= True
+
+    while pos < len(s1) and matches:
+        if list1[pos] == list2[pos]:
+            pos += 1
+        else:
+            matches = False
+
+    return matches
+
+print(anagram('hello', 'olleh'))
+print(anagram('mallam', 'llamaem'))
