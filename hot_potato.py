b18449e45aaead45a4fa90b3da53adbd67ba5b44
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-29 19:19:44 +0100

    A sims of the hot potato game using the queue data structure

---
+++ hot_potato.py
@@ -0,0 +1,20 @@
+from queue import Queue
+
+def hot_potato(name_list, num):
+  """
+  A program to simulate the hot potato game using the queue ADS
+  """
+  q = Queue()
+
+  for name in name_list:
+    q.enqueue(name)
+
+  while q.size() > 1:
+    for i in range(num):
+      q.enqueue(q.dequeue())
+
+    q.dequeue()
+
+  return q.dequeue()
+
+print(hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7))
